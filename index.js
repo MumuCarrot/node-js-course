34e72262f478730fe605c257d977151c253a5b5b
vanichka <138333895+MumuCarrot@users.noreply.github.com>
2025-10-14 13:55:50 +0200

    bug fix

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     const url = req.url;
     if(url ==='/about'){
-        res.write('<h1>about us page<h1>');
+        res.write('<h1>about us page</h1>');
         res.end();
     }else if(url ==='/contact'){
-        res.write('<h1>contact us page<h1>');
+        res.write('<h1>contact us page</h1>');
         res.end();
     }else{
-        res.write('<h1>Hello World!<h1>');
-        res.write('<h2>My name Ivan<h2>');
+        res.write('<h1>Hello World!</h1>');
+        res.write('<h2>My name Ivan</h2>');
         res.end();
     }
 });
